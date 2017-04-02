e0bb6d4625c5d7c87a4df90cbc3d9ad7c02ddb3f
Mateusz Mulcan <jefrey1986@o2.pl>
2017-04-02 21:57:53 +0200

    carousel first version

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+
+	setInterval(function() {
+		$(carouselList).animate({marginLeft:-400},500,function() {
+			//$(lastItem).after($(firstItem));
+			$(carouselList).find("li:last").after($(carouselList).find("li:first"))
+			$(carouselList).css({marginLeft:0});
+		})
+	},3000);
+});  
