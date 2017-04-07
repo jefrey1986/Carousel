005b2cae74c654efb67577c987cd7c8de442547c
Mateusz Mulcan <jefrey1986@o2.pl>
2017-04-07 13:23:09 +0200

    add slide button

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,41 @@
 $(function(){
 	var carouselList = $("#carousel ul");
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
+	var leftButton = $("#left");
+	var rightButton = $("#right")
 
 	setInterval(function() {
 		$(carouselList).animate({marginLeft:-400},500,function() {
-			//$(lastItem).after($(firstItem));
-			$(carouselList).find("li:last").after($(carouselList).find("li:first"))
+			var firstItem = carouselList.find("li:first");
+			var lastItem = carouselList.find("li:last");
+			$(lastItem).after($(firstItem));
+			//$(carouselList).find("li:last").after($(carouselList).find("li:first"))
 			$(carouselList).css({marginLeft:0});
 		})
-	},3000);
+	},5000); 
+
+	function slideLeft(){
+		$(carouselList).animate({marginLeft:-400},500,function() {
+			var firstItem = carouselList.find("li:first");
+			var lastItem = carouselList.find("li:last");
+			$(lastItem).after($(firstItem));
+			$(carouselList).css({marginLeft:0});
+		})
+	};
+
+	function slideRight(){
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		$(lastItem).before($(firstItem));
+		$(carouselList).css({marginLeft:-400});
+		$(carouselList).animate({marginLeft:0},500);
+	};
+
+	leftButton.click(function(){
+		slideLeft();
+	}); 
+
+	rightButton.click(function(){
+		slideRight();
+	}); 
+
 });  
